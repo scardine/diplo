eb118dade750486ef8ba6cfafaf1ed5bf97add86
paulos <paulo@3mw.com.br>
2016-11-12 19:28:45 -0200

    local_settings

--- glossario/admin.py
+++ glossario/admin.py
@@ -5,6 +5,6 @@ from glossario.models import Termo
 
 class TermoAdmin(admin.ModelAdmin):
     search_fields = ('termo',)
-    prepopulated_fields = {'slug', ('termo',)}
+    prepopulated_fields = {'slug': ('termo',)}
 
 admin.site.register(Termo, TermoAdmin)
